2eefb8960a2c9f13cdfe3014bbc8fe7d18b8f072
JinHo Yoo <jhy7342@gmail.com>
2024-08-16 13:31:28 +0900

    ADD: node

--- vars/getBranchList.groovy
+++ vars/getBranchList.groovy
@@ -1,13 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(String repositoryURL){
-    def branches = []
-    try {
-        branches = withCredentials([gitUsernamePassword(credentialsId: 'ea380414-e517-45cb-91dc-882c64c04255', gitToolName: 'Default')]) {
-            return sh (script: "git ls-remote -h ${repositoryURL} | sed 's?.*refs/heads/??' ", returnStdout:true).trim()
+def call(String repositoryURL) {
+    node {
+        def branches = []
+        try {
+            branches = withCredentials([gitUsernamePassword(credentialsId: 'ea380414-e517-45cb-91dc-882c64c04255', gitToolName: 'Default')]) {
+                return sh(script: "git ls-remote -h ${repositoryURL} | sed 's?.*refs/heads/??' ", returnStdout: true).trim()
+            }
+        } catch (error) {
+            echo "[getBranchList call()] Error ::: ${error}"
         }
-    }catch (error){
-        echo "Error ::: ${error}"
+        return branches
     }
-    return branches
 }
