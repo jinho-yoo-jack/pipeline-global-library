7fba46df6ec12fa1129afe65b27036b3eb2d81c0
JinHo Yoo <jhy7342@gmail.com>
2024-08-16 13:07:28 +0900

    ADD: Common API

---
+++ vars/getBranchList.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def getGitHubBranchList(repositoryURL){
+    def branches = []
+    try {
+        branches = withCredentials([gitUsernamePassword(credentialsId: 'ea380414-e517-45cb-91dc-882c64c04255', gitToolName: 'Default')]) {
+            return sh (script: "git ls-remote -h ${repositoryURL} | sed 's?.*refs/heads/??' ", returnStdout:true).trim()
+        }
+    }catch (error){
+        echo "Error ::: ${error}"
+    }
+    return branches
+}
