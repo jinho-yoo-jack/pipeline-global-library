d3fcf4b37b8a4a7d2b5319118eed97cf865b1741
black <jhy7342@gmail.com>
2024-08-19 22:24:42 +0900

    ADD: Create Docker Image for Common API

---
+++ vars/createDockerImage.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+repository = "jhy7342/cicd-study"  //docker hub id와 repository 이름
+DOCKERHUB_CREDENTIALS = credentials('docker-hub-access-key') // jenkins에 등록해 놓은 docker hub credentials 이름
+def call(String srcDir, String destDir, String repositoryName) {
+    node {
+        def buildDir = "${srcDir}" + "/build/libs"
+        try {
+            sh "cp /var/jenkins_home/workspace/$buildDir/*.jar /var/jenkins_home/workspace/$destDir/" // war 파일을 현재 위치로 복사 
+            sh "cp /var/jenkins_home/workspace/$buildDir/Dockerfile /var/jenkins_home/workspace/$destDir/" // Dockerfile
+            sh "pwd"
+            sh "docker build --platform linux/amd64 -t $repository:$BUILD_NUMBER ."
+            sh "docker build --platform linux/amd64 -t $repository:latest ."
+        } catch (error) {
+            echo "[createDockerImage call()] Error ::: ${error}"
+        }
+    }
+}
