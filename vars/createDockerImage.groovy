86c388eaa0be978cc8e5a52ddd95131aa4247444
black <jhy7342@gmail.com>
2024-08-20 20:27:23 +0900

    MOD: All

--- vars/createDockerImage.groovy
+++ vars/createDockerImage.groovy
@@ -1,21 +1,13 @@
 #!/usr/bin/env groovy
 
-repository = "jhy7342/cicd-study"  //docker hub id와 repository 이름
-DOCKERHUB_CREDENTIALS = credentials('docker-hub-access-key') // jenkins에 등록해 놓은 docker hub credentials 이름
-def call(String jenkinsHomePath, String srcDir, String repositoryName) {
+def call(String srcDir, String repositoryName) {
     node {
-         def destDir = sh (
-                        script: 'pwd',
-                        returnStdout: true
-                    ).trim() + "/"
-        echo "Destination Dir ::: ${destDir}"
-        echo "Source Dir ::: ${srcDir}"
-        echo "Repository ::: ${repositoryName}"
+        def home = "${JENKINS_HOME}"
+        def destDir = "${WORKSPACE}"
         def buildDir = "${srcDir}" + "/build/libs"
         try {
-            sh "cp $jenkinsHomePath/$buildDir/*.jar $destDir" // war 파일을 현재 위치로 복사 
-            sh "cp $jenkinsHomePath/$srcDir/Dockerfile $destDir" // Dockerfile
-            sh "pwd"
+            sh "cp -rf ${home}/$buildDir/build $destDir" // war 파일을 현재 위치로 복사 
+            sh "cp ${home}/$srcDir/Dockerfile $destDir" // Dockerfile
             sh "docker build --platform linux/amd64 -t $repository:$BUILD_NUMBER ."
             sh "docker build --platform linux/amd64 -t $repository:latest ."
         } catch (error) {
