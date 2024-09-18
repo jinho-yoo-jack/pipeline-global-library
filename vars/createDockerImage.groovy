46f53059bc418032480f6a45fbc0262c8ae57f7a
jinho-yoo-jack <jhy7342@gmail.com>
2024-09-18 18:31:14 +0900

    MOD: docker image process

--- vars/createDockerImage.groovy
+++ vars/createDockerImage.groovy
@@ -10,8 +10,7 @@ def call() {
         try {
             //sh "cp -rf ${home}/$buildDir $destDir" // war 파일을 현재 위치로 복사 
             //sh "cp ${home}/$srcDir/Dockerfile $destDir" // Dockerfile
-            sh "docker build --platform linux/amd64 -t $DOCKERHUB_REPOSITORY:$BUILD_NUMBER ."
-            sh "docker build --platform linux/amd64 -t $DOCKERHUB_REPOSITORY:latest ."
+            sh "docker build --platform linux/amd64 -t $DOCKERHUB_REPOSITORY ."
         } catch (error) {
             echo "[createDockerImage call()] Error ::: ${error}"
         }
